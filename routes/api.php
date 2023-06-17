b3a6b212bd3d9b32507f49edb2dbb998e41db5c5
Hunter Skrasek <hunterskrasek@me.com>
2023-06-17 00:51:11 -0500

    Add API routes and first LiveWire view
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\API\AddAttributes;
+use App\Http\Controllers\API\CreateEntity;
+use App\Http\Controllers\API\ViewEntity;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +19,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/entities/{entity:ulid}', ViewEntity::class)
+        ->name('entities.view');
+
+    Route::post('/entities', CreateEntity::class)
+        ->name('entities.create');
+
+    Route::post('/entities/{entity:ulid}/attributes', AddAttributes::class)
+        ->name('entities.attributes.create');
 });
